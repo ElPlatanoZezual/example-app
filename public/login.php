1516bbb78ab0ff0b7cdead38b6d10abd206cd1f5
ElPlatanoZezual <89117490+ElPlatanoZezual@users.noreply.github.com>
2023-10-05 03:01:13 -0300

    Initial commit

---
+++ public/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Iniciar sesion</title>
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <section class="vh-100" style="background-color: #919191;">
+            <div class="container py-5 h-100">
+              <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                  <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+          
+                      <h3 class="mb-5">Iniciar sesion</h3>
+          
+                      <div class="form-outline mb-4">
+                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+                        <label class="form-label" for="typeEmailX-2">Email</label>
+                      </div>
+          
+                      <div class="form-outline mb-4">
+                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                        <label class="form-label" for="typePasswordX-2">Contraseña</label>
+                      </div>
+          
+                     <!--checkbox
+                        <div class="form-check d-flex justify-content-start mb-4">
+                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                        <label class="form-check-label" for="form1Example3"> Remember password </label>
+                      </div>-->
+          
+                      <button class="btn btn-primary btn-lg btn-block" type="submit">Iniciar sesion</button>
+          
+
+          
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+    </body>
